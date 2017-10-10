f43a33d662e21f8c498542f179bd60842c24e7a1
karlarado <alexmkarla@gmail.com>
2017-10-09 21:39:37 -0400

    added highlight

---
+++ CheckerBoard Assignment 5/CheckerBoard/Scripts/Home.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function ()
+{
+    var cells = $(".cell");
+    var colorCount = 0;
+
+    for (var i = 0; i < cells.length; i++)
+    {
+        var cell = $(cells[i]);
+        var isDark = colorCount % 2 == 0;
+        var isNextRow = (i + 1) % 8 == 0;
+        colorCount += isNextRow ? 2 : 1;
+        cell.css("background-color", isDark ? "navy" : "white");
+    }
+
+   $(".cell").click(function()
+    {
+        $(this).css("border", "1px solid yellow");
+    }
+    )
+    
+
+});
