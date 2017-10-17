22602063a1f5667be8e4ec26fb3ed21f2dfe8ca1
karlarado <alexmkarla@gmail.com>
2017-10-16 22:44:13 -0400

    created JSON

---
+++ Assignment 7/scripts.js
@@ -0,0 +1,48 @@
+var main = {};
+
+main.Planets = {
+    "galaxy": "Milky Way",
+    "system": "Solar",
+    "planets": [
+        {
+            "name": "Mercury",
+            "color": "grey",
+            "moons": 0
+        },
+        {
+            "name": "Venus",
+            "color": "yellow",
+            "moons": 0
+        },
+        {
+            "name": "Earth",
+            "color": "blue",
+            "moons": 1
+        },
+        {
+            "name": "Mars",
+            "color": "red",
+            "moons": 2
+        },
+        {
+            "name": "Jupiter",
+            "color": "orange",
+            "moons": 67
+        },
+        {
+            "name": "Saturn",
+            "color": "gold",
+            "moons": 62
+        },
+        {
+            "name": "Uranus",
+            "color": "blue",
+            "moons": 27
+        },
+        {
+            "name": "Neptune",
+            "color": "blue",
+            "moons": 13
+        }
+    ]
+}
