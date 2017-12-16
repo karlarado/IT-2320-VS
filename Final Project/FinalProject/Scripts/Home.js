7efbbb8d594f9896c483ad09ff0f10cd311b96e4
karlarado <alexmkarla@gmail.com>
2017-12-15 21:50:08 -0500

    ajax updates

--- Final Project/FinalProject/Scripts/Home.js
+++ Final Project/FinalProject/Scripts/Home.js
@@ -1,1 +1,57 @@
-﻿
+﻿var Home = {};
+
+//variables defined for logIn
+
+Home.logIn = function() {
+
+    var username;
+    var password;
+
+    var username = $(".logIn.username").val();
+    var password = $(".logIn.password").val();
+
+    $.ajax({
+        url: "Home/Login",
+        data: {
+            "Username": username,
+            "Password": password
+        }
+}
+
+//variables defined for account creation
+Home.createAccount = function() {
+
+    var username;
+    var password;
+    var email;
+    var reEmail;
+
+    var username = $(".create.username").val();
+    var password = $(".create.password").val();
+    var email = $(".create.email").val();
+    var reEmail = $(".create.reEmail").val();
+
+    $.ajax({
+        url: "Home/CreateAccount",
+        data: {
+            "Username": username,
+            "Password": password,
+            "EmailAdd": email,
+            "EmailCon": reEmail
+        }
+
+}
+
+//functions called
+
+$(document).ready(function () {
+
+        $(".createButton").click(function()
+        {
+            Home.createAccount()
+        });
+
+        $(".logInButton").click(function () {
+            Home.logIn()
+        });
+    });
